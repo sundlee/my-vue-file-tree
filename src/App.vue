390cfc6d325de54bb73f511d88c3187f3e6ff985
sundlee <sundlee@gmail.com>
2022-04-13 07:39:43 +0900

    화면 분할 추가

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,72 @@
 <template>
-  <div id="app">
-    <file-browser-tree 
+  <div id="app" class="split">
+    <div id="split-0" class="content">
+      <vue-file-tree 
         id="file-tree"
         ref="filetree"
         class="column"
         @nodeClick="nodeClick"
         @nodeDoubleClick="nodeDoubleClick"
-        @nodeDrop="nodeDrop">
-
+        @nodeDrop="nodeDrop"
+      >
         <template slot="context-menu">
           <div @click="doDashboard">Dashboard</div>
           <div @click="doCustomers">Customers</div>
         </template>
-
-    </file-browser-tree>
+      </vue-file-tree >
+    </div>
+    <div id="split-1" class="content">
+      <ul>
+        <li>sizes: {{ sizes }} </li>
+        <li>gutterSize: {{ gutterSize }} </li>
+        <li>minSize: {{ minSize }} </li>
+        <li>maxSize: {{ maxSize }} </li>
+        <li>cursor: {{ cursor }} </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // const path = require('path');
 // const util = require('util');
-import FileBrowserTree from 'vue-file-tree';
+import Split from 'split.js';
+import VueFileTree from 'vue-file-tree';
 
 export default {
   name: 'App',
   components: {
-    'file-browser-tree': FileBrowserTree,
+    'vue-file-tree': VueFileTree,
   },
-  // data() {
-  //   return {
-  //     nodes: [
-  //       {title: 'Item1', isLeaf: true},
-  //       {title: 'Item2', isLeaf: true, data: { visible: false }},
-  //       {title: 'Folder1'},
-  //       {
-  //         title: 'Folder2', isExpanded: true, children: [
-  //           {title: 'Item3', isLeaf: true},
-  //           {title: 'Item4', isLeaf: true, data: { isModified: true }}
-  //         ]
-  //       }
-  //     ],
-  //   };
-  // },
-  // created() {
-  //   messageBus.$on('file', (fn, stat) => { 
-  //       this.$refs.filetree.addPathToTree(fn, stat, false);
-  //   });
-  //   messageBus.$on('directory', (fn, stat) => { 
-  //       this.$refs.filetree.addPathToTree(fn, stat, true);
-  //   });
-  // },
+  data() {
+    return {
+      sizes: [20, 80],
+      gutterSize: 10,
+      minSize: [150, 300],
+      maxSize: [350, Infinity],
+      cursor: 'col-resize',
+    };
+  },
   mounted() {
+    let sizes = localStorage.getItem('split-sizes')
+    if (sizes) {
+      sizes = JSON.parse(sizes);
+    } else {
+      sizes = [20, 80];
+    }
+    this.sizes = sizes;
+
+    Split(['#split-0', '#split-1'], {
+      sizes,
+      gutterSize: this.gutterSize,
+      minSize: this.minSize,
+      maxSize: this.maxSize,
+      cursor: this.cursor,
+      onDragEnd: function (sizes) {
+        localStorage.setItem('split-sizes', JSON.stringify(sizes))
+      },
+    });
+
     this.$refs.filetree.addPathToTree('template\\index.js', {}, false);
     this.$refs.filetree.addPathToTree('template\\log.png', {}, false);
     this.$refs.filetree.addPathToTree('src\\handler.py', {}, false);
@@ -60,47 +76,74 @@ export default {
   },
   methods: {
     nodeClick(event, node) {
-        // console.log(`nodeClick ${util.inspect(node)}`);
-        console.log('nodeClick: ', node);
+      // console.log(`nodeClick ${util.inspect(node)}`);
+      console.log('nodeClick: ', node);
     },
     nodeDoubleClick(node) {
-        // console.log(`nodeDoubleClick ${util.inspect(node)}`);
-        console.log('nodeDoubleClick: ', node);
+      // console.log(`nodeDoubleClick ${util.inspect(node)}`);
+      console.log('nodeDoubleClick: ', node);
     },
     nodeDrop(node) {
-        // console.log(`nodeDrop ${util.inspect(node)}`);
-        console.log('nodeDrop: ', node);
+      // console.log(`nodeDrop ${util.inspect(node)}`);
+      console.log('nodeDrop: ', node);
     },
     doCustomers() {
-        console.log(`doCustomers`);
-        this.$refs.filetree.contextMenuIsVisible = false;
+      console.log(`doCustomers`);
+      this.$refs.filetree.contextMenuIsVisible = false;
     },
     doDashboard() {
-        console.log(`doDashboard`);
-        this.$refs.filetree.contextMenuIsVisible = false;
+      console.log(`doDashboard`);
+      this.$refs.filetree.contextMenuIsVisible = false;
     }
-  }
+  },
 }
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+/* * {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+} */
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+}
 
-  #wrapper {
-    height: 100vh;
-    width: 100vw;
-  }
+/* #wrapper {
+  height: 100vh;
+  width: 100vw;
+} */
 
-  #file-tree, #file-info-view {
-    height: 100%;
-    overflow: scroll;
-  }
+#file-tree, #file-info-view {
+  height: 100%;
+  overflow: scroll;
+}
+
+body {
+  height: 100vh;
+}
+.content {
+	padding: 8px;
+	border: 1px solid #c0c0c0;
+	box-shadow: inset 0 1px 2px #e4e4e4;
+	background-color: #fff;
+	
+	height: 500px;
+}
+.split {
+  display: flex;
+  flex-direction: row;
+}
+.gutter {
+  background-color: #eee;
+  background-repeat: no-repeat;
+  background-position: 50%;
+}
+.gutter.gutter-horizontal {
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==');
+  cursor: col-resize;
+}
 </style>
